a0b47ae950ee0de34a5bfa8e09b8e8513563738d
ozgeb <ozge.bykask@outlook.com>
2022-03-20 00:35:09 +0300

    create movie and director structs

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+type Movie struct {
+	ID string `json:"id"`
+	Isbn string `json:"isbn"`
+	Title string `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	Firstname string `json:"firstname"`
+	Lastname string `json:"lastname"`
+}
+
+var moviesList []Movie
+
+func main(){
+	fmt.Println(len(moviesList))
+}
